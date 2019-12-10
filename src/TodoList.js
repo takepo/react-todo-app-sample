2e9023e18b15cdb2442ab919c47bb902e1b79feb
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-12-11 02:06:59 +0900

    fix: ListをMaterial-uiを使って置き換え

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function TodoList() {
   return (
-    <>
-      <div>プログラミング</div>
-      <div>お買い物</div>
-    </>
+    <List>
+      <ListItem>
+        <ListItemText primary="プログラミング" />
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="お買い物" />
+      </ListItem>
+    </List>
   );
 }
 
