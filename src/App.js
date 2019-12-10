de751ab96f52f6459020cebdfb801b784f69cd1f
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-12-11 01:55:02 +0900

    feat: ListComponentの仮作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import TodoList from './TodoList';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>TODOアプリ</h1>
+      <div>
+        <h1>TODOアプリ</h1>
+        <TodoList />
+      </div>
     );
   }
 }
 
 export default App;
+
 
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <>
+      <div>プログラミング</div>
+      <div>お買い物</div>
+    </>
+  );
+}
+
+export default TodoList;
+
